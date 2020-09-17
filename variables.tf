126c977e7e352d96253f13d3f7942af204324595
santosh kumar <40539714+santuvytla@users.noreply.github.com>
2020-09-17 11:48:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 variable "aws_region" {}
 variable "aws_profile" {}
 variable "localip" {}
-variable "db_instance_class" {}
-variable "dbname" {}
-variable "dbuser" {}
-variable "dbpassword" {}
 variable "key_name" {}
 variable "public_key_path" {}
 variable "domain_name" {}
